7b2ba7ceb5539489a3a24d73ff15bb389d74a1dc
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-06-05 16:12:05 -0600

    feat: Added vault get API

---
+++ src/app/(api)/api/vault/route.ts
@@ -0,0 +1,24 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
+const prisma = new PrismaClient()
+
+
+export async function GET(
+    req: Request,
+) {
+    try {
+        const user = await currentUser()
+
+        const vaultUser = await prisma.user.findUnique({
+            where: {
+                id: user?.id,
+            }
+        })
+        
+        return NextResponse.json(vaultUser?.vault)
+    } catch (error) {
+        console.error("Error getting vault:", error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
