51922e35d7851e1314680c049ed77db0fe37f51c
cvs0 <cvs0@lockscript.dev>
2024-10-29 17:37:20 -0600

    Layout fix

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import type {Metadata} from "next";
 import {Montserrat} from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 
 const inter = Montserrat({ subsets: ["latin"] });
 
